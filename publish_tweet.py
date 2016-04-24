949553d9b5fd2202e17c3c1053d9cf92a392f023
Manuel Garrido <manuel.garrido.pena@gmail.com>
2016-04-24 20:47:07 +0200

    Update publish_tweet.py

--- publish_tweet.py
+++ publish_tweet.py
@@ -9,17 +9,13 @@ MESSAGE_TEMPLATE = 'Good Morning! Right now there are {} Potentially Hazardous A
 
 def status_update(status, n_pha, media):
     """Updates status, with a potential orbit plot"""
-    CONSUMER_KEY = '1RfuCAPIhU9Daeih2Ed1g7MqU'
-    CONSUMER_SECRET = 'oS482yLCzUOihzbBMiHrqt3I8kWgBdtQI3ca8Jyv5uKmJxyttk'
-    ACCESS_KEY = '724152610094211072-pSJOOj8FMqLle3h6HPsaEysMCCTxT1V'
-    ACCESS_SECRET = 'LTD0l1CQDy7F5Hfko5ddtnBH2hu98q4UwopgvPE2nsC9r'
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
 
     message = MESSAGE_TEMPLATE.format(n_pha)
     if media:
-        api.update_with_media(media, status=status)
+        api.update_with_media(media, status=message)
     else:
         api.update_status(message)
     return "Tweet Published"
